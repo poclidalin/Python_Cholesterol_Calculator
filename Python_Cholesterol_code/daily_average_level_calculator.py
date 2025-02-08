0bac718cb7ab4dcfa1da4b1a226f5a05128cc066
poclidalin <poclidalin@yahoo.com>
2025-02-08 22:32:58 +0000

    Delete Python_Cholesterol_code directory

--- Python_Cholesterol_code/daily_average_level_calculator.py
+++
@@ -1,8 +0,0 @@
-def daily_average_level_calculator(avg_level):
-    # Categorize the cholesterol level based on the average.
-    if avg_level >= 200:
-        return "HIGH LEVEL"
-    elif avg_level < 120:
-        return "LOW LEVEL"
-    else:
-        return "NORMAL LEVEL"
--- Python_Cholesterol_code/utils.py
+++
@@ -1,5 +0,0 @@
-def calculate_average(values):
-    # Return 0 if the list is empty to avoid division by zero.
-    if not values:
-        return 0
-    return sum(values) / len(values)
